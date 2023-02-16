8f08440e7640f563d80df1390b64260a7cb6ecb7
NathyAlem22 <nathyalem22@gmail.com>
2023-02-16 15:04:00 -0800

    script that generates the assembly code (Intel syntax) of a C code and save it in an output file.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * stderr
+ * Return:0 if runs correctly
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
