531e773e899092ebae33f241db5a5b957e8c77d9
NathyAlem22 <nathyalem22@gmail.com>
2023-02-16 14:24:13 -0800

    C program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * The sizeof operator gives the amount of storage, in bytes
+ * Return: 0 if run properly
+ */
+int main(void)
+{
+	sizeof(char);
+	sizeof(int);
+	sizeof(long int);
+	sizeof(long long int);
+	sizeof(float);
+	return (0);
+}
