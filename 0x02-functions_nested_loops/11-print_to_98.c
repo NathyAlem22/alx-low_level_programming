ea24a74701d4b9b67c88401b70647b9e735b7d59
NathyAlem22 <nathyalem22@gmail.com>
2023-02-21 08:25:08 -0800

    adding

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,27 +6,20 @@
  * @n: the number to start counting from to 98
  * Return: void returns nothing.
  */
-void print_to_98(int n)
-{
-	int i;
 
+void print_to_98(int n)
+
+{
 	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d, ", i);
-		}
-	}
-	else if (n == 98)
-	{
-		printf("%d", n);
+		for (n = n; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
 	}
 	else
 	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d, ", i);
-		}
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
 	}
-	printf("\n");
 }
