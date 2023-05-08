f54f5b68c99feadd144c78aad982a90590276a3e
NathyAlem22 <nathyalem22@gmail.com>
2023-05-08 14:54:19 -0700

    updateing file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,34 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: If the function fails or filename is NULL - 0.
- *         s/w - the actual number of bytes the function can read and print.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+
+int create_file(const char *filename, char *text_content)
 {
-	ssize_t s, k, w;
-	char *buff;
+	int o, w, len = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
-		return (0);
-
-	s = open(filename, O_RDONLY);
-	k = read(s, buff, letters);
-	w = write(STDOUT_FILENO, buff, r);
-
-	if (s == -1 || k == -1 || w == -1 || w != k)
+	if (text_content != NULL)
 	{
-		free(buff);
-		return (0);
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	free(buff);
-	close(s);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	return (w);
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
