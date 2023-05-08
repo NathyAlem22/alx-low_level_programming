e090cfe259adeded365f0a1282923c359d12ff3b
NathyAlem22 <nathyalem22@gmail.com>
2023-05-07 23:20:55 -0700

    updating files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,21 +11,21 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int o, k, lengths = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (lengths = 0; text_content[lengths];)
+			lengths++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	k = write(o, text_content, lengths);
 
-	if (o == -1 || w == -1)
+	if (o == -1 || k == -1)
 		return (-1);
 
 	close(o);
