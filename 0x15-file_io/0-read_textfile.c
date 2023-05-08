2786befe2ef0e5aa53652dea4c07553f5c7de5bc
NathyAlem22 <nathyalem22@gmail.com>
2023-05-07 23:11:07 -0700

    updating files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,32 +8,32 @@
  *           function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
+ *         s/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	ssize_t s, k, w;
+	char *buff;
 
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	s = open(filename, O_RDONLY);
+	k = read(s, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	if (s == -1 || k == -1 || w == -1 || w != k)
 	{
-		free(buffer);
+		free(buff);
 		return (0);
 	}
 
-	free(buffer);
-	close(o);
+	free(buff);
+	close(s);
 
 	return (w);
 }
