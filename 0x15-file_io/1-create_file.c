ccfa0cd0922a7e25fd456199cc888853e2078864
NathyAlem22 <nathyalem22@gmail.com>
2023-05-07 23:16:49 -0700

    updating files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,21 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int o, k, lengths = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		for (lengths = 0; text_content[lengths];)
+			lengths++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	k = write(o, text_content, lengths);
 
-	if (o == -1 || w == -1)
+	if (o == -1 || k == -1)
 		return (-1);
 
 	close(o);
